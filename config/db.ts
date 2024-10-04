4a4c49ce0290305bb55511f0a85dbf2f296ff559
S3rpa <sergiobarbera1@gmail.com>
2024-10-04 05:20:06 +0200

    validations

--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',       
-  user: 'root',            
-  database: 'HotelMiranda',
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'nombre_de_tu_base_de_datos',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
